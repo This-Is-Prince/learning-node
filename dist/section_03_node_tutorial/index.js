5ad2ca332fac254a599330a7d793d2ae218b0723
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-08-08 19:31:08 +0530

    feat(ch_016): mind grenade complete

--- dist/section_03_node_tutorial/index.js
+++ dist/section_03_node_tutorial/index.js
@@ -1,4 +1,5 @@
 "use strict";
 // require('./ch_010_cli')
 // require('./ch_012_globals')
-require('./ch_014_first_module');
+// require('./ch_014_first_module')
+require('./ch_016_mind_grenade');
--- src/section_03_node_tutorial/index.ts
+++ src/section_03_node_tutorial/index.ts
@@ -1,3 +1,4 @@
 // require('./ch_010_cli')
 // require('./ch_012_globals')
-require('./ch_014_first_module')
+// require('./ch_014_first_module')
+require('./ch_016_mind_grenade')
