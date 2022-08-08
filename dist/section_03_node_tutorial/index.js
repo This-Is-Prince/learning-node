c4d1de5f233a5c118931539fc1bfa635cecc97d8
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-08-08 20:08:36 +0530

    feat(ch_019): path complete

--- dist/section_03_node_tutorial/index.js
+++ dist/section_03_node_tutorial/index.js
@@ -4,4 +4,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // require('./ch_012_globals')
 // require('./ch_014_first_module')
 // require('./ch_016_mind_grenade')
-require("./ch_018_os_module");
+// import './ch_018_os_module'
+require("./ch_019_path_module");
--- src/section_03_node_tutorial/index.ts
+++ src/section_03_node_tutorial/index.ts
@@ -2,4 +2,5 @@
 // require('./ch_012_globals')
 // require('./ch_014_first_module')
 // require('./ch_016_mind_grenade')
-import './ch_018_os_module'
+// import './ch_018_os_module'
+import './ch_019_path_module'
